18efd117dbb3c199befdd351b31fc1693d0b3ac9
Mwesigye John Bosco <johnboscomwesigye@gmail.com>
2023-03-31 17:07:22 +0300

    Add more assert statement on savings account

--- karate-tests/src/test/java/org/apache/fineract/KarateTestApplicationTest.java
+++ karate-tests/src/test/java/org/apache/fineract/KarateTestApplicationTest.java
@@ -36,7 +36,7 @@ class KarateTestApplicationTest implements BaseKarate {
         System.setProperty("testpass", testpass.orElse("password"));
         System.setProperty("tenantId", tenantId.orElse("default"));
 
-        Karate karate = new Karate().path("classpath:features/portfolio/loans");
+        Karate karate = new Karate().path("classpath:features/");
         karate.outputHtmlReport(true);
         return karate;
     }
