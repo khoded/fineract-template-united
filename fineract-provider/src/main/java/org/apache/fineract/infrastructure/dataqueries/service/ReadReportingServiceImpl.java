f0d3c0b5535235adf6a3c75fe64509c808c16dce
Faheem Ahmad <faheem@fiter.io>
2023-09-19 00:39:51 +0500

    UC-160 UC-159

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/dataqueries/service/ReadReportingServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/dataqueries/service/ReadReportingServiceImpl.java
@@ -205,7 +205,9 @@ public class ReadReportingServiceImpl implements ReadReportingService {
     }
 
     private String getSql(final String name, final String type) {
-        final String encodedName = sqlInjectionPreventerService.encodeSql(name);
+        //final String encodedName = sqlInjectionPreventerService.encodeSql(name);
+        //Commenting above encoding because it replaces "-"" with -/ in report name
+
         final String encodedType = sqlInjectionPreventerService.encodeSql(type);
 
         final String inputSql = "select " + encodedType + "_sql as the_sql from stretchy_" + encodedType + " where " + encodedType
@@ -214,12 +216,12 @@ public class ReadReportingServiceImpl implements ReadReportingService {
         final String inputSqlWrapped = this.genericDataService.wrapSQL(inputSql);
 
         // the return statement contains the exact sql required
-        final SqlRowSet rs = this.jdbcTemplate.queryForRowSet(inputSqlWrapped, encodedName);
+        final SqlRowSet rs = this.jdbcTemplate.queryForRowSet(inputSqlWrapped, name);
 
         if (rs.next() && rs.getString("the_sql") != null) {
             return rs.getString("the_sql");
         }
-        throw new ReportNotFoundException(encodedName);
+        throw new ReportNotFoundException(name);
     }
 
     @Override
